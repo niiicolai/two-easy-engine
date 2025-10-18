4e1264eb66f438d601eb41721ee1d02643e7e16a
niiicolai <niiicolai@gmail.com>
2025-10-18 22:13:10 +0200

    ensure generate-api-docs.sh automatically creates the api dir. if it doesn't exist

--- scripts/generate-api-docs.sh
+++ scripts/generate-api-docs.sh
@@ -1,4 +1,13 @@
+
+OUTPUT_DIR="docs/api"
+
+# Create output dir if it doesn't exists
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir $OUTPUT_DIR
+fi
+
+# Create markdown file for JavaScript files
 for f in src/**/*.js; do
   base=$(basename "$f" .js)
-  npx jsdoc2md "$f" > "docs/api/$base.md"
+  npx jsdoc2md "$f" > "$OUTPUT_DIR/$base.md"
 done
